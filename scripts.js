dfd9154a3038ad814944391a186c040cc81981a8
Sunneva <sunneva_@msn.com>
2020-06-24 15:09:02 +0000

    started styles for scss, more accessible html

--- scripts.js
+++ scripts.js
@@ -1,8 +1,1 @@
 //Contains functionality!
-
-console.log(document.querySelector(".SettingButton"));
-document.querySelector(".SettingButton").addEventListener("click", function(){
-    console.log("clickeroony");
-    console.log(document.querySelector(".SettingsMenu"));
-    document.querySelector(".SettingsMenu").styles.display = "block";
-})
