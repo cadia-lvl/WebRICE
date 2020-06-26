d96862c377eb01df01124dacd436d5588a5b18d3
Sunneva <sunneva_@msn.com>
2020-06-26 12:03:59 +0000

    more func

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 //Contains functionality!
 
+import { text } from "./resources/text/is.mjs";
+
 //Globals
 const MAIN_CONTAINER = document.getElementById("webrice");
-const SETTINGS_CLASSNAME = "settingsMenu";
+const SETTINGS_ID = "settingsCont";
+
 
 //_________________________
 function createHighlightSection(){
@@ -14,7 +17,7 @@ function createHighlightSection(){
     isHighlightInput.setAttribute("id", highlightStatus);
     isHighlightInput.setAttribute("type", "checkbox");
     isHighlightInput.checked = true;
-    isHighlightLabel.appendChild(document.createTextNode("Lita bakgrunn settninga:"))
+    isHighlightLabel.appendChild(document.createTextNode(text.settings.colorBackg))
     isHighlightLabel.setAttribute("for", highlightStatus);
     //add children
     isHighlightSection.appendChild(isHighlightLabel);
@@ -22,37 +25,58 @@ function createHighlightSection(){
     return isHighlightSection;
 }
 
+function createPharagraphs(text){
+    let description = document.createElement("p");
+    return description.appendChild(document.createTextNode(text));
+}
 
 function createInformationSection(){
     let isInfoSection = document.createElement("section");
 
     let h3Element = document.createElement("h3");
-    h3Element.appendChild(document.createTextNode("Hvað er veflesari?"));
-    
-    
+    h3Element.appendChild(document.createTextNode(text.settings.whatIsHead));
+    isInfoSection.appendChild(h3Element);
 
-    
+    let textCont = document.createElement("div");
+    textCont.appendChild(createPharagraphs(text.settings.whatIsPhara1));
+    //TODO: add more text here
+
+    isInfoSection.appendChild(textCont);
+    return isInfoSection;
+
 }
 
-//determines weather menu is active or not
-function hasMenu(childArr){
-    for(item in childArr){
-        if(childArr[item].classList.contains(SETTINGS_CLASSNAME)){
-            return true
-        }
-    }
-    return false;
+function createSettingsHeader(){
+    const headerCont = document.createElement("div");
+
+    headerCont.setAttribute("id", "settingsHeader");
+    const exitButton = document.createElement("button")
+    exitButton.setAttribute("id", "closeSettings");
+
+    const exitImg = document.createElement("img");
+    exitImg.setAttribute("src", "./Images/close-white-18dp.svg");
+    exitImg.setAttribute("alt", "loka");
+
+    exitButton.appendChild(exitImg);
+    headerCont.appendChild(exitButton);
+    return headerCont;
 }
 
 function createSettings(){
-    let settings = document.createElement("div");
-    settings.classList.add(SETTINGS_CLASSNAME);
+    const settingsCont = document.createElement("div");
+    settingsCont.setAttribute("id", SETTINGS_ID);
+
+    settingsCont.appendChild(createSettingsHeader())
+
+    const settings = document.createElement("div");
+    settings.classList.add("settingsMenu");
     
     let mainHeading = document.createElement("h2")
-    mainHeading.appendChild(document.createTextNode("Stillingar"))
+    mainHeading.appendChild(document.createTextNode(text.settings.mainHead));
     settings.appendChild(mainHeading);
 
     settings.appendChild(createHighlightSection());
+    settings.appendChild(createInformationSection());
 
     
     let submitButton = document.createElement("input");
@@ -60,17 +84,18 @@ function createSettings(){
     submitButton.setAttribute("value", "vista");
     settings.appendChild(submitButton);
 
-    MAIN_CONTAINER.appendChild(settings);
+    settingsCont.appendChild(settings);
+
+    MAIN_CONTAINER.appendChild(settingsCont);
+
+    document.getElementById("closeSettings").addEventListener("click", destroySettings);
 } 
 
 function destroySettings(){
-    Array.from(MAIN_CONTAINER.childNodes).forEach( child => {
-        if(child.classList.contains(SETTINGS_CLASSNAME)){
-            MAIN_CONTAINER.removeChild(child);
-        }
-    });
+    MAIN_CONTAINER.removeChild(document.getElementById(SETTINGS_ID));
 }
 
+//On click functions
 document.querySelector(".SettingButton").addEventListener("click", function() {
-    hasMenu(Array.from(MAIN_CONTAINER.childNodes)) ? destroySettings() : createSettings();
+    document.getElementById(SETTINGS_ID) ? destroySettings() : createSettings();
 });
