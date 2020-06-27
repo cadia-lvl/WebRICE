514c4e196cec82c915a5ed91f92809b073b541fa
Sunneva <sunneva_@msn.com>
2020-06-27 20:21:31 +0000

    module w. fixed exit

--- scripts.js
+++ scripts.js
@@ -4,7 +4,14 @@ import { Settings } from "./resources/modules/settings.mjs"
 const SettingsId = "settingsCont";
 const settingsMod = new Settings(document.getElementById("webrice"), SettingsId);
 
+function makeSettings(){
+    settingsMod.createSettings();
+    document.getElementById("closeSettings").addEventListener("click", function() {
+        settingsMod.destroySettings();
+    });
+}
+
 //Creates the settings when settings button is pushed
 document.querySelector(".SettingButton").addEventListener("click", function() {
-    document.getElementById(SettingsId) ? settingsMod.destroySettings() : settingsMod.createSettings();
+    document.getElementById(SettingsId) ? settingsMod.destroySettings() : makeSettings();
 });
