5005acf1a114b38990f06542750b9798a95349d6
Sunneva <sunneva_@msn.com>
2020-07-01 16:59:09 +0000

    added download

--- mainButtons.mjs
+++ mainButtons.mjs
@@ -30,12 +30,13 @@ export class MainButtons{
         earImg.setAttribute("src","./Images/hearing-white-36dp.svg");
         earImg.setAttribute("alt","Mynd af hlustandi eyra");
         baseCont.appendChild(earImg);
-
         baseCont.appendChild(this.createImageButton("ListenButton","./Images/play_arrow-white-36dp.svg", "Spilunar takki", "Hlusta á grein"));
         WRManage.appendChild(baseCont);
         WRManage.appendChild(this.createImageButton("PauseButton","./Images/pause-white-36dp.svg", "Pásu takki", "Pása lestur"));
         WRManage.appendChild(this.createImageButton("StopButton","./Images/stop-white-36dp.svg", "Stöðvunar takki", "Stoppa lestur"));
         WRManage.appendChild(this.createImageButton("SpeedButton","./Images/speed-white-36dp.svg", "Stillingar takki fyrir hraða lesturs", "Stilla hraða"));
+        
+        WRManage.appendChild(this.createImageButton("DownloadButton", "./Images/vertical_align_bottom-white-36dp.svg", "niðurhals takki", "Hlaða niður lestri"));
         WRManage.appendChild(this.createImageButton("SettingButton","./Images/settings-white-36dp.svg", "Stillinga takki", "Stillingar"));
         this.mainCont.appendChild(WRManage);
     }
