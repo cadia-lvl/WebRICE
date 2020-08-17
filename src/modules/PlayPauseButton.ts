f04256e9608ab56cb3d04e856b0bf23dea7dfdaf
Sunneva Þorsteinsdóttir <32647733+SunnevaThorsteins@users.noreply.github.com>
2020-08-17 13:50:37 +0000

    Update PlayPauseButton.ts

--- src/modules/PlayPauseButton.ts
+++ src/modules/PlayPauseButton.ts
@@ -9,7 +9,7 @@ export class PlayPauseButton extends Button {
   secondIcon: Icon;
   /**
    *
-   * @param {Icon} Icon - The main Icon of button
+   * @param {Icon} icon - The main Icon of button
    * @param {Icon} secondIcon - The second Icon of button
    * @param {Icon} toggleIcon - The toggleable Icon of button
    * @param {string} alt - The alt text of button
@@ -32,7 +32,8 @@ export class PlayPauseButton extends Button {
   }
 
   /**
-   * toggles icon and toggleIcon
+   * Sets the visible main icon of the play button to be toggleIcon and stores the other icon.
+   * For example: If the play icons is visible it changes to the pause icon and vice versa.
    */
   private toggleIcons(): void {
     // Changes play to pause and pause to play
