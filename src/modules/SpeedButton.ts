bb50834563de90df75a953fa0bc3d90850a6f702
judyfong <judyfong@users.noreply.github.com>
2020-08-30 18:10:22 +0000

    Move html and css in index to the styling file and SpeedButton's createhtml

--- src/modules/SpeedButton.ts
+++ src/modules/SpeedButton.ts
@@ -6,6 +6,8 @@ import {Icon} from './icons';
  */
 export class SpeedButton extends MainButton {
     currentSpeed: number
+    // TODO: use enumerator for speedSettings;
+    speedSettings = [0.5, 0.75, 1, 1.25, 1.5, 1.75, 2];
     /**
      *
      * @param {Icon} icon - icon on button
@@ -45,5 +47,38 @@ export class SpeedButton extends MainButton {
      */
     private setCurrentSpeed(speed: number): void {
       this.currentSpeed = speed;
+    }
+    /**
+     * Create the html for the speed button group
+     * The button group consists of two parts.
+     * The first part is the button and the icon.
+     * The second part is the list of speed options.
+     * Creates the button html
+     * @return {HTMLDivElement} - button html
+     */
+    public createHTML(): HTMLDivElement {
+      const speedButtonGroup = document.createElement('div');
+      speedButtonGroup.classList.add('speedButtonGroup');
+      const button = document.createElement('div');
+      button.id = this.id;
+      button.setAttribute('role', 'button');
+      button.setAttribute('alt', this.alt);
+      button.setAttribute('title', this.title);
+      button.setAttribute('tabindex', '0');
+      button.setAttribute('aria-expanded', 'true');
+
+      if (this.classes !== '') button.classList.add(this.classes);
+
+      button.appendChild(this.buttonIcon.svg);
+      speedButtonGroup.appendChild(button);
+      const speedOptions = document.createElement('ul');
+      speedOptions.classList.add('webriceMainSpeed');
+      this.speedSettings.forEach( (speed) => {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(speed.toString()));
+        speedOptions.appendChild(li);
+      });
+      speedButtonGroup.appendChild(speedOptions);
+      return speedButtonGroup;
     }
 }
