4f30beb60f3dcac8f32b62aa73ebb76373c9a669
judyfong <judyfong@users.noreply.github.com>
2020-09-04 13:20:54 +0000

    speed button - Select the new playbackRate from the 7 speed options

--- src/modules/SpeedButton.ts
+++ src/modules/SpeedButton.ts
@@ -25,15 +25,17 @@ export class SpeedButton extends MainButton {
     }
 
     /**
-     * onClick listener for speed button
-     * Display the reading speed options
+     * Display or hide the reading speed options depending on the current
+     * display option
      */
-    toggleReadingSpeeds(): void {
+    toggleReadingSpeedsMenu(): void {
       const readingSpeedsElement = document
           .getElementById(this.speedSelectId) as HTMLUListElement;
       if ( readingSpeedsElement.style.display === 'block') {
         this.hideReadingSpeeds();
       } else {
+        (document.getElementById(this.id) as HTMLDivElement)
+            .setAttribute('aria-expanded', 'true');
         readingSpeedsElement.style.display = 'block';
       }
     }
@@ -45,6 +47,8 @@ export class SpeedButton extends MainButton {
       const readingSpeedsElement = document
           .getElementById(this.speedSelectId) as HTMLUListElement;
       readingSpeedsElement.style.display = 'none';
+      (document.getElementById(this.id) as HTMLDivElement)
+          .setAttribute('aria-expanded', 'false');
     }
 
     /**
@@ -71,11 +75,60 @@ export class SpeedButton extends MainButton {
     }
 
     /**
-     * Sets current reading speed
-     * @param {number} speed what current speed will be
+     * Sets current reading speed for the SpeedButton class
+     * @param {number} speed - what current speed will be
      */
     private setCurrentSpeed(speed: number): void {
       this.currentSpeed = speed;
+    }
+
+    /**
+     * Sets the styling and accessibility attributes for the current speed
+     * @param {HTMLLIElement} selectedSpeed - what current speed will be
+     */
+    private setActiveSpeedAttributes(selectedSpeed: HTMLLIElement): void {
+      selectedSpeed.setAttribute('aria-checked', 'true');
+      selectedSpeed.setAttribute('aria-selected', 'true');
+      selectedSpeed.classList.add('active');
+    }
+
+    /**
+     * Sets the styling and accessibility attributes for the other speeds
+     * @param {HTMLLIElement} aSpeed - what current speed will be
+     */
+    private setNonActiveSpeedAttributes(aSpeed: HTMLLIElement): void {
+      aSpeed.setAttribute('aria-checked', 'false');
+      aSpeed.setAttribute('aria-selected', 'false');
+      aSpeed.classList.remove('active');
+    }
+
+    /**
+     * Enables user to change reading speed
+     * onclick function for each of the speed options
+     * Sets current reading speed for the SpeedButton class based on the speed
+     * selected and sets it in the audio player's playbackRate
+     * Removes the active class from the former speed and adds it to the
+     * current speed
+     * @param {MouseEvent} event - the click that triggered this function
+     * @listens
+     */
+    public changePlaybackRate(event: MouseEvent): void {
+      const selectSpeed = event.target as HTMLLIElement;
+      if (selectSpeed && selectSpeed.matches('li')) {
+        const newPlaybackRate = Number(selectSpeed.innerText);
+        if (newPlaybackRate >= this.speedSettings[0] &&
+           newPlaybackRate <= this.speedSettings[this.speedSettings.length-1]) {
+          const player = document.getElementById('webricePlayer') as
+              HTMLAudioElement;
+          this.setCurrentSpeed(Number(selectSpeed.innerText));
+          player.playbackRate = this.getCurrentSpeed();
+          // remove the active class on the former speed
+          this.setNonActiveSpeedAttributes(document.querySelector(
+              '#' + this.speedSelectId + ' > li.active') as HTMLLIElement);
+          // then put it on the new current speed
+          this.setActiveSpeedAttributes(selectSpeed);
+        }
+      }
     }
 
     /**
@@ -93,14 +146,12 @@ export class SpeedButton extends MainButton {
       button.setAttribute('alt', this.alt);
       button.setAttribute('title', this.title);
       button.setAttribute('tabindex', '0');
-      button.setAttribute('aria-expanded', 'true');
+      button.setAttribute('aria-expanded', 'false');
       button.classList.add('speedButtonGroup');
       if (this.classes !== '') button.classList.add(this.classes);
 
       button.appendChild(this.buttonIcon.svg);
 
-      // TODO: is webriceSelectId really needed? can it be combined with
-      // webriceMainSpeed?
       // Create the playbackRate options popup
       const speedOptions = document.createElement('ul');
       speedOptions.classList.add('webriceMainSpeed');
@@ -108,15 +159,16 @@ export class SpeedButton extends MainButton {
       this.speedSettings.forEach( (speed) => {
         const li = document.createElement('li');
         li.appendChild(document.createTextNode(speed.toString()));
+        li.title = speed.toString();
         li.setAttribute('role', 'option');
         if (this.getCurrentSpeed() === speed) {
-          li.classList.add('active');
-          li.setAttribute('aria-checked', 'true');
-          li.setAttribute('aria-selected', 'true');
+          this.setActiveSpeedAttributes(li);
         } else {
-          li.setAttribute('aria-checked', 'false');
-          li.setAttribute('aria-selected', 'false');
+          this.setNonActiveSpeedAttributes(li);
         }
+        li.addEventListener('click', (e) => {
+          this.changePlaybackRate(e);
+        });
         speedOptions.appendChild(li);
       });
       button.appendChild(speedOptions);
